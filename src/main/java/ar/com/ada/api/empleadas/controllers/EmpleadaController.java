e5c522b8a5b4553f2ae51524824a0b9b6eab9532
flordife <flor.difelice@gmail.com>
2021-06-24 00:47:16 -0300

    se crea infoEmpleadaNueva.java. Se agregan en el EmpleadaController los métodos traerEmpleada / crearEmpleada / getEmpleadaPorId / bajaEmpleada / obtenerEmpleadaPorId. Se agrega la anotación JsonIgnore

--- src/main/java/ar/com/ada/api/empleadas/controllers/EmpleadaController.java
+++ src/main/java/ar/com/ada/api/empleadas/controllers/EmpleadaController.java
@@ -1,14 +1,17 @@
 package ar.com.ada.api.empleadas.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import ar.com.ada.api.empleadas.entities.Empleada;
-import ar.com.ada.api.empleadas.services.EmpleadaService;
+import ar.com.ada.api.empleadas.entities.*;
+import ar.com.ada.api.empleadas.entities.Empleada.EstadoEmpleadaEnum;
+import ar.com.ada.api.empleadas.models.request.InfoEmpleadaNueva;
+import ar.com.ada.api.empleadas.models.response.GenericResponse;
+import ar.com.ada.api.empleadas.services.*;
 
 @RestController
 public class EmpleadaController {
@@ -16,9 +19,67 @@ public class EmpleadaController {
     @Autowired
     EmpleadaService service;
 
-    @GetMapping("/empleadas")
+    @Autowired
+    CategoriaService categoriaService;
+
+    @GetMapping("/empleados")
     public ResponseEntity<List<Empleada>> traerEmpleadas(){
-        return ResponseEntity.ok(service.traerEmpleadas());
+        List<Empleada> lista = service.traerEmpleadas();
+
+        return ResponseEntity.ok(lista);
     }
+
+    @PostMapping("/empleados")
+    public ResponseEntity<?> crearEmpleada(@RequestBody InfoEmpleadaNueva empleadaInfo) {
+        GenericResponse respuesta = new GenericResponse();
+
+        Empleada empleada = new Empleada();
+        empleada.setNombre(empleadaInfo.nombre);
+        empleada.setEdad(empleadaInfo.edad);
+        empleada.setSueldo(empleadaInfo.sueldo);
+        empleada.setFechaAlta(new Date());
+        
+        Categoria categoria = categoriaService.buscarCategoria(empleadaInfo.categoriaId);
+        empleada.setCategoria(categoria);
+        empleada.setEstado(EstadoEmpleadaEnum.ACTIVO);
+
+        service.crearEmpleada(empleada);
+        respuesta.isOk = true;
+        respuesta.id = empleada.getEmpleadaId();
+        respuesta.message = "La empleada fue creada con exito";
+        return ResponseEntity.ok(respuesta);
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleada> getEmpleadaPorId(@PathVariable Integer id){
+        Empleada empleada = service.buscarEmpleada(id);
+
+        return ResponseEntity.ok(empleada);
+    }
+
+    //Detele/empleados/{id} --> Da de baja un empleado poniendo el campo estado en "baja"
+    // y la fecha de baja que sea el dia actual.
+    @DeleteMapping("/empleados/{id}")
+    public ResponseEntity<GenericResponse> bajaEmpleada(@PathVariable Integer id){
+
+        service.bajaEmpleadaPorId(id);
+
+        GenericResponse respuesta = new GenericResponse();
+
+        respuesta.isOk = true;
+        respuesta.message = "La empleada fue dada de baja con exito";
+
+        return ResponseEntity.ok(respuesta);
+
+    }
+
+    //Get /empleados/categorias/{catId} --> Obtiene la lista de empleados de una categoria.
+    @GetMapping("/empleados/categorias/{catId}")
+    public ResponseEntity<List<Empleada>> obtenerEmpleadasPorCategoria(@PathVariable Integer catId){
+        
+        List<Empleada> empleadas = service.traerEmpleadaPorCategoria(catId);
+        return ResponseEntity.ok(empleadas);
+    }
+
     
 }
--- src/main/java/ar/com/ada/api/empleadas/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/empleadas/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package ar.com.ada.api.empleadas.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,14 @@ public class CategoriaService {
         repository.save(categoria);
     }
 
+    public Categoria buscarCategoria(Integer categoriaId){
 
+        Optional<Categoria> resultado =  repository.findById(categoriaId);
+        Categoria categoria = null;
+
+        if (resultado.isPresent())
+            categoria = resultado.get();
+
+        return categoria;
+    }
 }
